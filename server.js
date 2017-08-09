838f42f59912ee865262eb846e36395bdbbb8d0b
Alex <a.m.dussaq@gmail.com>
2017-08-08 20:24:28 -0500

    add in server 2

--- server.js
+++ server.js
@@ -189,23 +189,23 @@
     });
 
 
-    // var server2 = restify.createServer({
-    //     accept: ['application/json', 'image/tif', 'image/png']
-    // });
-    // server2.use(restify.queryParser());
-    // server2.use(restify.CORS({}));
+    var server2 = restify.createServer({
+        accept: ['application/json', 'image/tif', 'image/png']
+    });
+    server2.use(restify.queryParser());
+    server2.use(restify.CORS({}));
 
-    // server2.get(/\/img\/kinome\/?.*/, restify.serveStatic({
-    //     directory: "./server_imgs"
-    // }));
+    server2.get(/\/img\/kinome\/?.*/, restify.serveStatic({
+        directory: "./server_imgs"
+    }));
 
 
 
-    // /*The following is for the external db*/
-    // server2.get("/db/1.0.0/:collection_name", grabDbName);
-    // server2.get("/db/1.0.0/:collection_name/:doc_id", grabDocument);
-    // server2.listen(80, function () {
-    //     console.log('%s listening at %s', server2.name, server2.url);
-    // });
+    /*The following is for the external db*/
+    server2.get("/db/1.0.0/:collection_name", grabDbName);
+    server2.get("/db/1.0.0/:collection_name/:doc_id", grabDocument);
+    server2.listen(8080, function () {
+        console.log('%s listening at %s', server2.name, server2.url);
+    });
 
 }());
